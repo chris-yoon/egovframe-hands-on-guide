da63937757bb73092370e88fe52cceeb5dd89867
Chris Yoon <chris.cw.yoon@gmail.com>
2025-06-04 10:21:14 +0900

    mysql connector 8.0.33 amended

--- lab10-1-java-sql/src/lab/JDBCExample.java
+++ lab10-1-java-sql/src/lab/JDBCExample.java
@@ -12,7 +12,7 @@ public class JDBCExample {
 	public static void main(String[] args) {
 
 		try {
-				Class.forName("com.mysql.jdbc.Driver");
+				Class.forName("com.mysql.cj.jdbc.Driver");
 				System.out.println("Driver Loaded");
 			} catch (ClassNotFoundException e) {
 				System.out.println("Can not find driver class");
